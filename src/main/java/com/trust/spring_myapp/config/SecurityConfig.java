60165fb0e98bb8eed6625f7f44665dff03d104e5
suu2626 <154961142+suu2626@users.noreply.github.com>
2024-05-23 11:57:27 +0900

    Add files via upload

--- src/main/java/com/trust/spring_myapp/config/SecurityConfig.java
+++ src/main/java/com/trust/spring_myapp/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -33,5 +39,21 @@ public class SecurityConfig {
             .anyRequest().authenticated()
         );
         return http.build();
+    }
+
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	@Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("test@email.com")
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER")
+                .build();
+        manager.createUser(user);
+        return manager;
     }
 }
